108b45868e246e0f1cb33a358f6aac1391111271
oleg.gusev <gusevoleg@gmail.com>
2022-12-08 09:06:30 +0300

    db

--- test_key.py
+++ test_key.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     from main import searchy_key
     from core.models import DzenUser, UserDescription, Post, PostContent
 
-    list_resp = searchy_key(requests.session(), "spb")
+    list_resp = searchy_key(requests.session(), "Санкт")
     user_models = []
     user_description_models = []
     post_models = []
